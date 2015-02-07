b7d187089f3deb01d9c6cc02372597c5fa3c4806
Adam Gresh <adam.gresh@modernizingmedicine.com>
2015-02-07 12:56:30 -0500

    Add classes to represent field elements
    Add classes to hold dimensions for tote and chute which will be
    important for doing things like moving the arms up and down.

---
+++ Generated/DirtyMecanums/src/org/usfirst/frc3932/field/Tote.java
@@ -0,0 +1,13 @@
+package org.usfirst.frc3932.field;
+
+
+/**
+ *  TOTE is 26.9 in. long,16.9 in. wide, 12.1 in. tall
+ * 
+ *
+ */
+public class Tote {
+	public static final double HEIGHT_INCHES = 12.1;
+	public static final double WIDTH_INCHES = 16.9;
+	public static final double LENGTH_INCHES = 26.9;
+}
---
+++ Generated/DirtyMecanums/src/org/usfirst/frc3932/field/ToteChute.java
@@ -0,0 +1,18 @@
+package org.usfirst.frc3932.field;
+
+/**
+ * 
+ * >b>Each TOTE CHUTE opening is 1 ft. 3.25 in. tall by 1 ft. 6.5 in. wide</b>, 
+ * with the bottom of the opening located 1 ft. 6.5 in. above the carpet.
+ * The TOTE CHUTE is installed behind the TOTE CHUTE opening 
+ * (i.e. on the outside of the FIELD) and consists of an 2 ft. 6 in. long
+ * ramp of HDPE, at a 22 degree incline, with a upper edge that is 2 ft. 6.625 in. 
+ * from the floor. The ramp is covered with a shield of clear polycarbonate. A clear polycarbonate CHUTE DOOR is installed over the TOTE CHUTE opening and prevents a TOTE in the TOTE CHUTE from sliding out into the playing FIELD. Once a TOTE is loaded in to the TOTE CHUTE, the CHUTE DOOR can be opened using a handle located on either side of the CHUTE DOOR. This allows a TOTE loaded in the TOTE CHUTE to slide onto the FIELD.
+ *
+ */
+public class ToteChute {
+	public static final double TOP_EDGE_DIST_FROM_FLOOR_INCHES = (2 * 12) + .65d;
+	public static final double BOTTOM_EDGE_DIST_FROM_FLOOR_INCHES = (1 * 12) + .65d;
+	public static final double WIDTH_INCHES = (1 * 12) + 6.5d;
+	public static final double HEIGHT_INCHES = (1 * 12) + 3.25d;
+}
