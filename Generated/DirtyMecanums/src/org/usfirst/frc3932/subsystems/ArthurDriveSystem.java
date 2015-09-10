8991a247ea372eb61cc40b9a5adf0f2ad670be88
zachsussman <zachary@zacharysussman.com>
2015-09-09 20:42:46 -0400

    Added modifications for Arthur's tank-drive system

--- Generated/DirtyMecanums/src/org/usfirst/frc3932/subsystems/ArthurDriveSystem.java
+++ Generated/DirtyMecanums/src/org/usfirst/frc3932/subsystems/ArthurDriveSystem.java
@@ -1,5 +1,12 @@
 package org.usfirst.frc3932.subsystems;
 
 public class ArthurDriveSystem extends DriveSystem {
-
+    private final static double FORWARD_DRIVE_SPEED_SCALE = 0.70d;
+    private final static double TWIST_SPEED_SCALE = 0.70d;
+	public void drive(double x, double y, double twist){
+//		double forwardVector = adjust(y);
+//		double twistVector = Math.min(-adjust(x)*forwardVector + adjust(twist), 1);
+//    	mecanumDrive.mecanumDrive_Cartesian(0, forwardVector*FORWARD_DRIVE_SPEED_SCALE, twistVector*TWIST_SPEED_SCALE, 0);
+		mecanumDrive.arcadeDrive(adjust(x)*FORWARD_DRIVE_SPEED_SCALE, adjust(y)*TWIST_SPEED_SCALE);
+    }
 }
