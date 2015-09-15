edb40024a6fd02c49e70a89193cd272885b68b13
Adam Gresh <adam.gresh@modernizingmedicine.com>
2015-09-14 23:15:20 -0400

    Adjust the drive system to be throttle and rotate left / right

--- Generated/DirtyMecanums/src/org/usfirst/frc3932/subsystems/ArthurDriveSystem.java
+++ Generated/DirtyMecanums/src/org/usfirst/frc3932/subsystems/ArthurDriveSystem.java
@@ -1,12 +1,14 @@
 package org.usfirst.frc3932.subsystems;
 
 public class ArthurDriveSystem extends DriveSystem {
-    private final static double FORWARD_DRIVE_SPEED_SCALE = 0.70d;
-    private final static double TWIST_SPEED_SCALE = 0.70d;
-	public void drive(double x, double y, double twist){
+	public static final double SCALE = .75;
+	
+    public void drive(double x, double y, double rawAxis3){
+    	
 //		double forwardVector = adjust(y);
 //		double twistVector = Math.min(-adjust(x)*forwardVector + adjust(twist), 1);
 //    	mecanumDrive.mecanumDrive_Cartesian(0, forwardVector*FORWARD_DRIVE_SPEED_SCALE, twistVector*TWIST_SPEED_SCALE, 0);
-		mecanumDrive.arcadeDrive(adjust(x)*FORWARD_DRIVE_SPEED_SCALE, adjust(y)*TWIST_SPEED_SCALE);
+		mecanumDrive.arcadeDrive(x * SCALE, rawAxis3 * SCALE);
+		///mecanumDrive.arcadeDrive(adjust(x)*FORWARD_DRIVE_SPEED_SCALE, adjust(y)*TWIST_SPEED_SCALE);
     }
 }
